3848b891cde9496f6c1501dd767b49a18090027d
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2023-05-06 15:26:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ uploaded_file = st.file_uploader("Upload the pitch in Powerpoint format", type=[
 
 if uploaded_file is not None:
   loader = UnstructuredPowerPointLoader(uploaded_file.name)
-  #pitch_data = loader.load()
+  pitch_data = loader.load()
   st.write("data:", uploaded_file.name)
   
 
